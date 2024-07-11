5084889b902840e8423d18cadc579e507547674e
neaimt <pas120344@gmail.com>
2024-07-11 16:59:03 +0900

    init project

---
+++ Sejong_Domitory/Sejong_Domitory/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sejong_Domitory
+//
+//  Created by 박근경 on 2024/07/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sejong_Domitory/Sejong_Domitory/Sejong_DomitoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sejong_DomitoryApp.swift
+//  Sejong_Domitory
+//
+//  Created by 박근경 on 2024/07/11.
+//
+
+import SwiftUI
+
+@main
+struct Sejong_DomitoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
